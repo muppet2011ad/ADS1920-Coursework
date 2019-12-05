cf498bfcc70345ab8a65e45951130d1361141976
James Townsend <j.a.townsend2000@gmail.com>
2019-12-05 17:17:26 +0000

    q1.py appears to pass tests and is now commented

--- q1.py
+++ q1.py
@@ -1,26 +1,29 @@
-def hash_quartic(keys):
-    table = ["-"]*23
-    
-    for key in keys:
-        hash = (4*key + 7) % 23
-        j = 0
-        while table[(hash+j**4) % 23] != "-":
-            j += 1
-        table[(hash+j**4) % 23] = key
+# q1.py written by James Townsend, December 2019
+# Tested using Python 3.7.4
 
-    return table
+def hash_quartic(keys):
+    table = ["-"]*23 # Initialise hash table array
+    
+    for key in keys: # Iterate over the keys we want to add
+        hash = (4*key + 7) % 23 # Calculate the hash of the key
+        j = 0 # Start counting j from zero
+        while table[(hash+j**4) % 23] != "-": # Iterate until we land on an empty bucket
+            j += 1 # Increment j
+        table[(hash+j**4) % 23] = key # Once we have our bucket, set the data
+
+    return table # Return the finished hash table
 
 def hash_double(keys):
-    table = ["-"]*23
+    table = ["-"]*23 # Initialise hash table array
 
-    for key in keys:
-        hash = (4*key + 7) % 23
-        if table[hash] == "-":
-            table[hash] = key
-        else:
-            offset = 17 - (key % 17)
-            while table[(hash + offset) % 23] != "-":
-                offset += 17 - (key % 17)
-            table[(hash + offset) % 23] = key
+    for key in keys: # Iterate over the keys we want to add
+        hash = (4*key + 7) % 23 # Calculate the hash of the key
+        if table[hash] == "-": # If the location at the hash is empty, then it's easy
+            table[hash] = key # We just set the data at that location to be our key
+        else: # If it's not easy
+            offset = 17 - (key % 17) # Calculate the offset using the secondary hash
+            while table[(hash + offset) % 23] != "-": # Whilst the location at hash + offset is not free
+                offset += 17 - (key % 17) # Calculate a new offset using the secondary hash
+            table[(hash + offset) % 23] = key # Once we have an empty bucket, store the key there
 
-    return table
+    return table # Return the finished hash table
