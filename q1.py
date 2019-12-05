eee431fa00840026ca20659c6ab8e632c5fbd2db
James Townsend <j.a.townsend2000@gmail.com>
2019-12-05 17:08:16 +0000

    hash_double function complete (untested)

--- q1.py
+++ q1.py
@@ -9,3 +9,18 @@ def hash_quartic(keys):
         table[(hash+j**4) % 23] = key
 
     return table
+
+def hash_double(keys):
+    table = ["-"]*23
+
+    for key in keys:
+        hash = (4*key + 7) % 23
+        if table[hash] == "-":
+            table[hash] = key
+        else:
+            offset = 17 - (key % 17)
+            while table[(hash + offset) % 23] != "-":
+                offset += 17 - (key % 17)
+            table[(hash + offset) % 23] = key
+
+    return table
