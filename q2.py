9286883ea2399a4198b1a9db90f9795d473e9adf
James Townsend <j.a.townsend2000@gmail.com>
2019-12-17 12:32:28 +0000

    Possibly viable recursive algorithm written but needs debugging

--- q2.py
+++ q2.py
@@ -3,7 +3,7 @@
 
 factMemo = {0:1,1:1} # This stores the results of all factorisations so we can quickly access them on-demand
 childCache = {} # This caches the results of all children so that we do not have to calculate them again when the same numbers come up
-descCache = {} # This caches the results of all descendants that we calculate, saving time on repeated tests (and when the same sequences comes up)
+descCache = {1:1} # This caches the results of all descendants that we calculate, saving time on repeated tests (and when the same sequences comes up)
 
 def fact(n): # Recursive function for factorising
     if n in factMemo: # If we have the factorial memoised
@@ -21,32 +21,22 @@ def getChild(num): # Function to get the child of a number
     childCache[num] = total # Cache the final result
     return total # Return the final result
 
+def getDescendants(n, seq=[]):
+    if n in seq:
+        return -1
+    if n in descCache:
+        return descCache[n]
+    res = 1 + getDescendants(getChild(n), seq + [n])
+    descCache[n] = res
+    return res
+
 def descendants(n1,n2,k): # Returns the number of integers [n1,n2) that have k descendants
     total = 0 # Keeps a total of the number of ints with k descendants
-    for n in range(n1,n2): # Iterate through the range
-        if n in descCache: # If we have already calculated the descendants for this number (perhaps from a previous test)
-            descs = descCache[n] # Get our descendants from the cache
-        else: # Otherwise
-            descs = [] # Start with an empty list for descendants
-            currentGen = getChild(n) # We will solve this by iterating through the generations - this gets the first child
-            while currentGen not in descs: # Whilst we're looking at a number that we haven't seen yet
-                descs.append(currentGen) # Append it to our list of descendants
-                if currentGen in descCache and getChild(currentGen) != currentGen: # If the number we're looking at has been cached and the number isn't its own child
-                    cached = descCache[currentGen] # Get the cached descendants
-                    if cached[len(cached)-1] in (n,currentGen): # Sometimes the last element of the cached sequence is the number we started with
-                        cached = cached[:-1] # If so, just leave it out
-                    if not set(descs).intersection(set(cached)): # Convert both our descendants and the cache to sets so we can find their intersection - if there are no common elements then we can use the cache
-                        descs = descs + cached # Append the cache
-                        break # If we've appended a cached list of descendants, then we don't need to iterate anymore
-                    else:
-                        currentGen = getChild(currentGen) # If we can't use the cache, find the next child
-                else:
-                    currentGen = getChild(currentGen) # If we can't use the cache, find the next child
-            descCache[n] = descs # Update the cache with the list of descendants we found
-            
-        if len(descs) == k: # If the length of the descendants list is k
-            total += 1 # Increment our counter
-    return total # Return the total at the end
+    for i in range(n1,n2):
+        descs = getDescendants(i)
+        if descs == k:
+            total += 1
+    return total
             
 def q2test():
     assert descendants(1,2,1) == 1
