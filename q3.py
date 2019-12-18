35e67bdb27be67837f9d87dc3f01ef399f812341
James Townsend <j.a.townsend2000@gmail.com>
2019-12-18 14:27:28 +0000

    LP2 works and both functions are commented up

--- q3.py
+++ q3.py
@@ -124,32 +124,39 @@ def LOP(string):
     return max(longest)
     
 def LP(string):
-    longestOdd = LOP(string)
+    longestOdd = LOP(string) # We already have a function for finding the longest odd, we might as well use it
 
-    longestEven = 0
+    longestEven = 0 # Keep a variable to track the longest even we've seen
 
-    for i in range(1,len(string)):
-        centreL = string[i-1]
-        centreR = string[i]
-        if centreL != centreR:
-            continue
+    for i in range(1,len(string)): # Iterate over the different centre points in the string (we have two for each value of i since this is an even palindrome)
+        centreL = string[i-1] # Our centre-left pointer is at i-1
+        centreR = string[i] # Our centre-right pointer is at i
+        if centreL != centreR: # If they don't equal each other
+            continue # There's no palindrome in site, so we might as well move on
         ptrL = i - 1
-        ptrR = i
-        while True:
-            if ptrL - 1 < 0 or ptrR + 1 > len(string) - 1:
-                break
+        ptrR = i # Create pointers for the left and right of the string
+        while True: # Iterate
+            if ptrL - 1 < 0 or ptrR + 1 > len(string) - 1: # If incrementing/decrementing either pointer would go out of range,
+                break # Break out of the loop
             ptrL -= 1
-            ptrR += 1
-            if string[ptrL] != string[ptrR]:
-                break
-        length = ptrR - ptrL + 1
-        if length > longestEven:
-            longestEven = length
+            ptrR += 1 # Otherwise we can safely update the pointers
+            if string[ptrL] != string[ptrR]: # If the characters at each pointer don't match, 
+                break # Break out of the loop
+        length = ptrR - ptrL + 1 # Once out of the loop, calculate the length of the palindrome based on pointer values
+        if length > longestEven: # If it's the longest one we've seen so far
+            longestEven = length # Store it
     
-    return max((longestEven,longestOdd))
+    return max((longestEven,longestOdd)) # Return whichever is longest out of the odd and even palindromes
 
         
-    
+def LP2(string):
+    longest = 0 # Keep track of the longest we get
+    for i in range(0, len(string)): # Iterate over the possible starting locations for our "loop" around the string
+        toTest = string[i:] + string[:i] # Construct a string composed of everything after i followed by everything before it
+        longTest = LP(toTest) # Get the longest palindrome in the string from LP
+        if longTest > longest: # If it's the longest one we've seen so far,
+            longest = longTest # Update the longest variable
+    return longest # Return the result
     
     
 def tests():
@@ -172,14 +179,18 @@ def tests():
         
     print("LP tests passed")
 
-    #assert LP2("a") == 1
-    #assert LP2("cbc") == 3
-    #assert LP2("abba") == 4
-    #assert LP2("abbac") == 5
-    #assert LP2("abcdefg") == 1
-    #assert LP2("abbabba") == 7
-    #assert LP2("banana") == 5
-    #assert LP2("abcdefgfhijkl") == 3
+    assert LP2("a") == 1
+    assert LP2("cbc") == 3
+    assert LP2("abba") == 4
+    assert LP2("abbac") == 5
+    assert LP2("abcdefg") == 1
+    assert LP2("abbabba") == 7
+    assert LP2("banana") == 5
+    assert LP2("abcdefgfhijkl") == 3
+
+    print("LP2 tests passed")
+
+    print("All tests passed")
     
 if __name__ == "__main__":
     tests()
