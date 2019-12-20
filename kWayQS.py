43e8861a21d1a8b5ca4ad9bc5167d58b900d020b
James Townsend <j.a.townsend2000@gmail.com>
2019-12-20 12:27:20 +0000

    Partition appears to be working though I was an idiot with my insertion sort

--- kWayQS.py
+++ kWayQS.py
@@ -22,27 +22,45 @@ def insertionsort(A):
         while y > 0 and A[y-1] > A[y]:
             tmp = A[y]
             A[y] = A[y-1]
-            A[y-1] = A[y]
+            A[y-1] = tmp
             y -= 1
         x += 1
     return A
    
     
 def partition(A, k):
-    # your code goes here
-    return ? # whatever you want it to return
+    pivots = insertionsort(A[:k]) # Select the first k elements to be our pivots (randomised quicksort) and sorts them (to make comparison easier)
+    nonpivots = A[k:]
+    partDict = {}
+    for pivot in pivots:
+        partDict[pivot] = []
+    partDict["end"] = []
+    for integer in nonpivots:
+        done = False
+        for pivot in pivots:
+            if integer < pivot:
+                partDict[pivot].append(integer)
+                done = True
+                break
+        if not done:
+            partDict["end"].append(integer)
+    partitions = []
+    for pivot in pivots:
+        partitions.append(partDict[pivot] + [pivot])
+    partitions.append(partDict["end"])
+    return partitions
+    
     
 
 def quicksort(A, k):
-    # your code goes here
-    return ? # return value must be argument list A in sorted
+    print(partition([1,7,2,6,3,4,5],k))
     
 
 def main():
-    k = int(sys.argv[1])
-    filename = sys.argv[2]
-    A = read_input(filename)
-    print(quicksort(A,k))
+    #k = int(sys.argv[1])
+    #filename = sys.argv[2]
+    #A = read_input(filename)
+    print(quicksort([],3))
     
 if __name__ == "__main__":
     main()
